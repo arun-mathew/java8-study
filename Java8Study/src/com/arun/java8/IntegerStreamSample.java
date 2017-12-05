2f250006ec1fafc603e9bd46ea90fe72fa6e2ddf
arun.mathew <arunkmathewulikkal@gmail.com>
2017-12-06 04:32:46 +0530

    java 8 study

---
+++ Java8Study/src/com/arun/java8/IntegerStreamSample.java
@@ -0,0 +1,36 @@
+package com.arun.java8;
+
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+public class IntegerStreamSample {
+
+	public static void main(String[] args) {
+		
+		System.out.println(isPrime(1));
+		System.out.println(isPrime(56));
+		System.out.println(isPrime(13));
+		System.out.println(isPrime(13));
+		System.out.println(isPrime(115));
+		System.out.println(isPrime(7));
+	}
+	
+	public static boolean isPrime(int number)
+	{
+		return number > 1 &
+				IntStream.range(2, number)
+				.noneMatch(index -> number % index == 0);
+				
+	}
+	
+	/*public static boolean isPrime2(int number)
+	{
+	    Predicate<Integer> isDivisible = divisor -> number % divisor == 0;
+		
+		return number > 1 &
+				IntStream.range(2, number)
+				.noneMatch(index -> isDivisible(index));
+				
+	}*/
+
+}
---
+++ Java8Study/src/com/arun/java8/StreamOne.java
@@ -0,0 +1,43 @@
+package com.arun.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamOne {
+
+	public static void main(String[] args) {
+		List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		System.out.println(values.stream()
+									.filter(x -> x > 3)
+									.filter(x -> x % 2 == 0)
+									.map(x -> x*2)
+									.findFirst()
+									.get()
+				);
+		System.out.println(values.stream()
+									.filter(StreamOne::isGreater)
+									.filter(StreamOne::isDivisible)
+									.map(StreamOne::operation)
+									.findAny()
+									.get()
+				
+				);
+	}
+		
+	public static boolean isGreater(int number)
+	{
+		return number > 3;
+	}
+	
+	public static boolean isDivisible(int number)
+	{
+		return number % 2 == 0;
+	}
+	
+	public static int operation(int number)
+	{
+		return number*2;
+	}
+
+}
