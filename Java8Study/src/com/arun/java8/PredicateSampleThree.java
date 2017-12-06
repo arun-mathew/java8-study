2c9deb2ecb7ec61cdc0a151bb76bde758661f8b2
arun.mathew <arunkmathewulikkal@gmail.com>
2017-12-06 06:01:08 +0530

    Using Function<Integer, Predicate<Integer>> insted of Predicate for more generalized prediction, Function will return a is greaterThan predicate of given input

--- Java8Study/src/com/arun/java8/PredicateSampleThree.java
+++ Java8Study/src/com/arun/java8/PredicateSampleThree.java
@@ -2,31 +2,34 @@ package com.arun.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class PredicateSampleThree {
 
 	public static void main(String[] args) {
 			List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-			
-			System.out.println(values.stream()
-										.filter(StreamOne::isGreater)
-										.filter(StreamOne::isDivisible)
-										.map(StreamOne::operation)
-										.findAny()
-										.get()
-					
-					);
-			
+						
 			Predicate<Integer> greaterThanThree = number -> number > 3 ;
 			
 			Predicate<Integer> divisibleByTwo = number -> number%2 == 0;
 			
+			//Another approach using predicate //
 			System.out.println(values.stream()
 										.filter(greaterThanThree)
 										.filter(divisibleByTwo)
 										.mapToInt(number -> operation(number))
 										.findAny()
+										.getAsInt());
+			//Get an integer pivot value and return an integer predicate // 
+			Function<Integer, Predicate<Integer>> isGreaterThan = pivot -> number -> number > pivot;
+			
+			//Yet another level from predicate using Function which return a predicate //
+			System.out.println(values.stream()
+										.filter(isGreaterThan.apply(5))
+										.filter(divisibleByTwo)
+										.mapToInt(number -> operation(number))
+										.findFirst()
 										.getAsInt());
 			
 		}
