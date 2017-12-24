1979fc6d43aa1a591fad629ac7542e07f7fb97ba
arun.mathew <arunkmathewulikkal@gmail.com>
2017-12-24 06:14:53 +0530

    excersise 2

--- Java8Study/src/com/arun/functionalPrograming/IntStream1.java
+++ Java8Study/src/com/arun/functionalPrograming/IntStream1.java
@@ -1,5 +1,7 @@
 package com.arun.functionalPrograming;
 
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class IntStream1 {
@@ -10,9 +12,16 @@ public class IntStream1 {
 		System.out.println(isPrime(8));
 		System.out.println(isPrime(5));
 		System.out.println(isPrime(7));
+		
+		
 	}
-
+		
 	private static boolean isPrime(int number) {
+		
+		//Imperative style of programiming
+		//mutability
+		//Telling the code , how to do//
+		//more error prone//
 		
 //		for(int i = 2; i< number; i++) {
 //			if(number % i == 0)
@@ -20,6 +29,11 @@ public class IntStream1 {
 //		}
 //		
 //		return number > 1 ;
+		
+		//Declerative style of programming//
+		//immutable
+		//Telling waht to do and let the code work on //
+		
 	    return number > 1 && IntStream.range(2, number)
 	    							  .noneMatch(index -> number % index == 0);
 		
